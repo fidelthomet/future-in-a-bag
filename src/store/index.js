3cd76c7c80b395c94815d858429ed9481bb1ad59
Fidel Thomet <me@fidelthomet.com>
2020-06-18 13:30:31 +0200

    📁 auto-load scenarios from folder

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import scenarios from 'dir-loader!@/assets/scenarios/scenarios.js' // eslint-disable-line import/no-webpack-loader-syntax
+console.log(Object.keys(scenarios).map(key => ({ hash: key.replace(/\.json$/, ''), ...scenarios[key].src })))
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
