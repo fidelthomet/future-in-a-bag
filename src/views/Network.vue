e9391742e19d7c8f3fe2693a36ca01172e6fd2fa
Fidel Thomet <me@fidelthomet.com>
2020-06-18 14:56:10 +0200

    📏 reisze

--- src/views/Network.vue
+++ src/views/Network.vue
@@ -1,18 +1,42 @@
 <template>
-  <div class="network">
-    <ThreeScene :width="512" :height="512"/>
+  <div class="network" v-resize:debounce.initial="resize">
+    <ThreeScene :width="width" :height="height"/>
   </div>
 </template>
 
 <script>
 import ThreeScene from '@/components/ThreeScene.vue'
+import resize from 'vue-resize-directive'
 export default {
   name: 'network',
   components: {
     ThreeScene
+  },
+  directives: {
+    resize
+  },
+  data () {
+    return {
+      width: 512,
+      height: 512
+    }
+  },
+  methods: {
+    resize (el) {
+      const rect = el.getBoundingClientRect()
+      this.width = rect.width
+      this.height = rect.height
+    }
   }
 }
 </script>
 <style lang="scss" scoped>
 @import "@/assets/style/global";
+
+.network {
+  position: absolute;
+  height: 100vh;
+  width: 100vw;
+  overflow: hidden;
+}
 </style>
