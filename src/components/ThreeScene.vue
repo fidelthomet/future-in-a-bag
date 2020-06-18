ef5ac1656f22724a1575564e5ce8f138ccc93056
Fidel Thomet <me@fidelthomet.com>
2020-06-18 16:28:04 +0200

    🖼scene setup

--- src/components/ThreeScene.vue
+++ src/components/ThreeScene.vue
@@ -5,8 +5,8 @@
 </template>
 
 <script>
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { Scene, WebGLRenderer, OrthographicCamera, Color, Group, DirectionalLight, Vector3 } from 'three'
+import { MapControls } from 'three/examples/jsm/controls/OrbitControls'
+import * as THREE from 'three'
 export default {
   name: 'three-scene',
   props: {
@@ -42,7 +42,6 @@ export default {
       camera: null,
       container: null,
       controls: null,
-      size: 1000,
       mouse: { x: 5000, y: 5000 }
     }
   },
@@ -55,48 +54,71 @@ export default {
     }
   },
   mounted () {
-    const { size, width, height } = this
-    const aspect = width / height
-    this.camera = new OrthographicCamera(size * aspect / -2, size * aspect / 2, size / 2, size / -2, 1, size * 2)
-    this.scene = new Scene()
-    this.renderer = new WebGLRenderer({ antialias: true })
-    this.container = new Group()
+    const { width, height } = this
+    this.camera = new THREE.OrthographicCamera(width / -2, width / 2, height / 2, height / -2, 1, 1000)
+    this.scene = new THREE.Scene()
+    this.renderer = new THREE.WebGLRenderer({ antialias: true })
+    this.container = new THREE.Group()
 
     const { camera, renderer, scene, $refs, animate, container } = this
-    camera.position.x = size
-    camera.position.y = size / 4
-    camera.lookAt(new Vector3(0, 0, 0))
+    camera.position.set(0, 0, 1000)
+    camera.lookAt(new THREE.Vector3(0, 0, 0))
     renderer.setSize(width, height)
     renderer.setPixelRatio(window.devicePixelRatio)
-    const dirLight = new DirectionalLight(0xffffff, 1)
+    const dirLight = new THREE.DirectionalLight(0xffffff, 1)
     dirLight.position.set(1, 5, 4)
     scene.add(dirLight)
     scene.add(container)
-    scene.background = new Color(0x020212)
+    scene.background = new THREE.Color(0x020212)
     $refs.three.appendChild(renderer.domElement)
 
-    this.controls = new OrbitControls(this.camera, renderer.domElement)
-    this.controls.minZoom = 1
-    this.controls.maxZoom = 10
-    this.controls.enablePan = false
+    this.controls = new MapControls(camera, renderer.domElement)
+    this.controls.enableDamping = true
+    this.controls.enableKeys = false
+    this.controls.enableRotate = false
+    this.controls.enableZoom = false
+    this.controls.dampingFactor = 0.2
+    this.controls.screenSpacePanning = true
+
+    this.controls.minDistance = 100
+    this.controls.maxDistance = 500
+
+    this.controls.maxPolarAngle = Math.PI / 2
+
+    var geometry = new THREE.BoxBufferGeometry(1, 1, 1)
+    geometry.translate(0, 0.5, 0)
+    var material = new THREE.MeshPhongMaterial({ color: 0xffffff, flatShading: true })
+
+    for (var i = 0; i < 500; i++) {
+      var mesh = new THREE.Mesh(geometry, material)
+      mesh.position.x = Math.random() * 1600 - 800
+      mesh.position.y = Math.random() * 1600 - 800
+      mesh.position.z = 0
+      mesh.scale.x = 20
+      mesh.scale.y = 20
+      mesh.scale.z = 20
+      mesh.updateMatrix()
+      mesh.matrixAutoUpdate = false
+      scene.add(mesh)
+    }
 
     animate()
   },
   methods: {
     animate (t = 0) {
       const { animate, scene, camera, renderer, controls } = this
-      camera.updateMatrixWorld()
-      controls.rotateSpeed = 1 / this.camera.zoom
+      // camera.updateMatrixWorld()
+      controls.update()
       renderer.render(scene, camera)
       requestAnimationFrame(animate)
     },
     resize () {
-      const { width, height, camera, renderer, size } = this
-      const aspect = window.innerWidth / window.innerHeight
-      camera.left = -size * aspect / 2
-      camera.right = size * aspect / 2
-      camera.top = size / 2
-      camera.bottom = -size / 2
+      const { width, height, camera, renderer } = this
+      console.log(camera.position.x, camera.position.y, camera.position.z)
+      camera.left = width / -2
+      camera.right = width / 2
+      camera.top = height / 2
+      camera.bottom = height / -2
       camera.updateProjectionMatrix()
       renderer.setSize(width, height)
     },
