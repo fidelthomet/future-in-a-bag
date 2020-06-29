e620d735f122388796520bb5748d348313302899
Fidel Thomet <me@fidelthomet.com>
2020-06-29 18:04:19 +0200

    🔧 fixed loading animation for cross browser

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
   <div id="app">
     <router-view/>
     <div v-if="!initialized" class="loader">
-      <svg width="400" height="400" viewBox="0 0 200 200">
+      <!-- <svg width="400" height="400" viewBox="0 0 200 200"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink">
         <g transform="translate(100 100)">
           <g class="anim dumb"><circle r="50" cx="50"/></g>
           <g class="anim rev smart"><circle r="50" cx="-100"/></g>
@@ -11,7 +13,33 @@
           <g class="anim smart"><circle r="50" cy="-150"/></g>
           <g class="anim rev smart"><circle r="50" cy="-50"/></g>
         </g>
-      </svg>
+      </svg> -->
+      <div class="loading">
+        <div class="anim dumb">
+          <div class="circle bg" style="transform: translateX(50px)"/>
+          <div class="circle dumb" style="transform: translateX(50px)"/>
+        </div>
+        <div class="anim rev smart">
+          <div class="circle bg" style="transform: translateX(-100px)"/>
+          <div class="circle smart" style="transform: translateX(-100px)"/>
+        </div>
+        <div class="anim dumb">
+          <div class="circle bg" style="transform: translateY(100px)"/>
+          <div class="circle dumb" style="transform: translateY(100px)"/>
+        </div>
+        <div class="anim rev dumb">
+          <div class="circle bg" style="transform: translateX(150px)"/>
+          <div class="circle dumb" style="transform: translateX(150px)"/>
+        </div>
+        <div class="anim smart">
+          <div class="circle bg" style="transform: translateY(-150px)"/>
+          <div class="circle smart" style="transform: translateY(-150px)"/>
+        </div>
+        <div class="anim rev smart">
+          <div class="circle bg" style="transform: translateY(-50px)"/>
+          <div class="circle smart" style="transform: translateY(-50px)"/>
+        </div>
+      </div>
       <span>running<br>scenarios</span>
     </div>
   </div>
@@ -44,15 +72,18 @@ export default {
   align-items: center;
   height: 100vh;
 
-  svg {
+  .loading {
     // margin-bottom: $spacing;
+    transform-box:fill-box;
     position: absolute;
-    overflow: visible;
+    // overflow: visible;
+    display: block;
+    background: $color-black;
+    transform: scale(1.5);
 
     .anim {
+      position: absolute;
       mix-blend-mode: difference;
-      @include tint(fill);
-
       animation-name: loading;
       animation-duration: 2s;
       animation-iteration-count: infinite;
@@ -66,7 +97,13 @@ export default {
 
       }
 
-      circle {
+      .circle {
+        position: absolute;
+        @include tint(background);
+        // mix-blend-mode: difference;
+        border-radius: 50px;
+        width: 100px;
+        height: 100px;
       }
     }
     @keyframes loading {
